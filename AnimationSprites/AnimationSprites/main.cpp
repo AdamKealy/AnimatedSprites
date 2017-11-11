d11d698a375a6abe220e9de74f1a42c395ea9441
c00213914 <c00213914@A310C034>
2017-11-11 15:42:25 +0000

    Start up

--- AnimationSprites/AnimationSprites/main.cpp
+++ AnimationSprites/AnimationSprites/main.cpp
@@ -1,35 +1,85 @@
-/// <summary>
-/// simple game loop for SFML[2.4.2]
-/// 
-/// @author Peter Lowe
-/// @date May 21017
-/// </summary>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "AnimatedSprite.h"
+#include "Player.h"
+#include "Input.h"
+#include "Debug.h"
 
-#ifdef _DEBUG 
-#pragma comment(lib,"sfml-graphics-d.lib") 
-#pragma comment(lib,"sfml-audio-d.lib") 
-#pragma comment(lib,"sfml-system-d.lib") 
-#pragma comment(lib,"sfml-window-d.lib") 
-#pragma comment(lib,"sfml-network-d.lib") 
-#else 
-#pragma comment(lib,"sfml-graphics.lib") 
-#pragma comment(lib,"sfml-audio.lib") 
-#pragma comment(lib,"sfml-system.lib") 
-#pragma comment(lib,"sfml-window.lib") 
-#pragma comment(lib,"sfml-network.lib") 
-#endif 
+using namespace std;
 
-
-#include "Game.h"
-
-/// <summary>
-/// main enrtry point
-/// </summary>
-/// <returns>true</returns>
 int main()
 {
-	Game game;
-	game.run();
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-	return 1;
-}
+	// Load a sprite to display
+	sf::Texture texture;
+	if (!texture.loadFromFile("assets\\grid.png")) {
+		DEBUG_MSG("Failed to load file");
+		return EXIT_FAILURE;
+	}
+
+	// Setup Players Default Animated Sprite
+	AnimatedSprite animated_sprite(texture);
+	animated_sprite.addFrame(sf::IntRect(3, 3, 84, 84));
+	animated_sprite.addFrame(sf::IntRect(88, 3, 84, 84));
+	animated_sprite.addFrame(sf::IntRect(173, 3, 84, 84));
+	animated_sprite.addFrame(sf::IntRect(258, 3, 84, 84));
+	animated_sprite.addFrame(sf::IntRect(343, 3, 84, 84));
+	animated_sprite.addFrame(sf::IntRect(428, 3, 84, 84));
+
+	// Setup the Player
+	Player player(animated_sprite);
+	Input input;
+	
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				// Close window : exit
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				{
+					input.setCurrent(Input::Action::LEFT);
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				{
+					input.setCurrent(Input::Action::RIGHT);
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+				{
+					input.setCurrent(Input::Action::UP);
+				}
+				break;
+			default:
+				input.setCurrent(Input::Action::IDLE);
+				break;
+			}
+		}
+
+		// Handle input to Player
+		player.handleInput(input);
+
+		// Update the Player
+		player.update();
+
+		// Clear screen
+		window.clear();
+
+		// Draw the Players Current Animated Sprite
+		window.draw(player.getAnimatedSprite());
+
+		// Update the window
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+};
